4d08d39345b860cbe2a9ff5ca04e04f5285d6541
Greg Lixandrão <contato@greglixandrao.com>
2024-02-23 19:35:13 -0300

    feat: add DataSeason record

---
+++ src/main/java/com/greglixandrao/screenmatchspring/model/DataSeason.java
@@ -0,0 +1,14 @@
+package com.greglixandrao.screenmatchspring.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataSeason(
+        @JsonAlias("Season")
+        Integer seasonNumber,
+        @JsonAlias("Episodes")
+        List<DataEpisode> episodes) {
+}
